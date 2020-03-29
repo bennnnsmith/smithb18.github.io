17a18050161b553e261814cf3d6b107fd9277f1e
smithb18 <39067570+smithb18@users.noreply.github.com>
2020-03-28 20:35:20 -0700

    updated front end

--- portfolio/src/components/HomePage.vue
+++ portfolio/src/components/HomePage.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="page-height">
-    <div>
-      >>> Home Page 
+
+    <div class="container">
+
+      <div class="row-fluid">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-6">asdsadadassadsad</div>
+            <div class="col-md-6">asdasdasdasdas</div>
+
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'homapage',
-
-}
+  name: "homapage"
+};
 </script>
 
 <style>
-
 </style>
